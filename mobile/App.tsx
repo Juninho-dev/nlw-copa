e0549f24c723881ab059b404ece2d83d8199c310
Juniel Filappi <juniel123123@outlook.com>
2022-11-01 10:25:36 -0400

    first comit

---
+++ mobile/App.tsx
@@ -0,0 +1,41 @@
+import { StyleSheet } from "react-native";
+import {
+  useFonts,
+  Roboto_400Regular,
+  Roboto_500Medium,
+  Roboto_700Bold,
+} from "@expo-google-fonts/roboto";
+
+import { NativeBaseProvider, StatusBar } from "native-base";
+
+import { THEME } from "./src/styles/theme";
+import { Loading } from "./src/components/Loading";
+import { SignIn } from "./src/screens/SignIn";
+
+export default function App() {
+  const [fontsLoaded] = useFonts({
+    Roboto_400Regular,
+    Roboto_500Medium,
+    Roboto_700Bold,
+  });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      {fontsLoaded ? <Loading /> : <SignIn />}
+    </NativeBaseProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
